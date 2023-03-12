73b4789032d66c116c65a37d4ae3c0e8a905c840
EdwinGuty <edwingutrou@unisabana.edu.co>
2023-03-12 14:02:47 -0500

    multiplicar terminado

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Multiplicar {
+public class Multiplicar {
+    public void multiplicar(int numero_1,int numero_2){
+        System.out.println(numero_1*numero_2);
+    }
 }
