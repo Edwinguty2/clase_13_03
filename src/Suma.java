bd9ba8b4212f3b3d599aec2dedc6d75b2b90e6f5
EdwinGuty <edwingutro@unisabana.edu.co>
2023-03-13 11:49:49 -0500

    Sumar con tres numeros

--- src/Suma.java
+++ src/Suma.java
@@ -1,5 +1,5 @@
 public class Suma {
-    public void sumar(int numero_1,int  numero_2){
-        System.out.println(numero_1+numero_2);
+    public void sumar(int numero_1,int  numero_2, int numero_3){
+        System.out.println(numero_1+numero_2+numero_3);
     }
 }
