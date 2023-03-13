0af8a59d17ce7f854d76fb580e66fde1b4fdd5e9
EdwinGuty <edwingutro@unisabana.edu.co>
2023-03-13 13:21:21 -0500

    multiplicar con 4 variables

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -1,5 +1,5 @@
 public class Multiplicar {
-    public void multiplicar(int numero_1,int numero_2){
-        System.out.println(numero_1*numero_2);
+    public void multiplicar(int numero_1,int numero_2, int  numero_3, int numero_4){
+        System.out.println(numero_1*numero_2*numero_3*numero_4);
     }
 }
